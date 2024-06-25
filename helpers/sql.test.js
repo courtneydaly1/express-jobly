38553fad53c480f42c570e59bfb57c92719a9056
Courtney Daly <courtneydaly1@gmail.com>
2024-06-25 10:39:50 -0400

    made test for sqlForPatialUpdate

---
+++ helpers/sql.test.js
@@ -0,0 +1,26 @@
+const { describe } = require("node:test")
+const { sqlForPartialUpdate} = require("./sql")
+
+describe("sqlForPartialUpdate", ()=>{
+    test("Correct with one input", ()=>{
+        const result = sqlForPartialUpdate(
+            {f1: "value1"},
+            {f1: "f1", fF2: "f2"}        
+        );
+        expect(result).toEqual({
+            setCols: "\"f1\"= $1",
+            values: ["value1"],
+        });
+    });
+
+    test("Correct with 2 inputs", ()=>{
+        const results = sqlForPartialUpdate(
+            {f1: "value1", jsF2: "value2"},
+            {jsF2: "f2"}
+        );
+        expect(result).toEqual({
+            setCols: "\"f1\"=$1, \"f2\"=$2",
+            values: ["value1", "value2"]
+        });
+    });
+});
